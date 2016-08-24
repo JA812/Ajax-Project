b78970715de4e26baa1bc7db05253a88ba7a9932
JA812 <johanna.asmuss@gmail.com>
2016-08-24 10:00:03 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,9 +13,54 @@ function loadData() {
 
     // load streetview
 
-    // YOUR CODE GOES HERE!
 
-    return false;
-};
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    //load New York Times Articles
+    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=0fa786e945f94870bad1858afc173a78'
+    $.getJSON(nytimesURL, function(data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+    articles = data.response.docs;
+    for (var i=0; i < articles.lenght; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class="article">' +
+        '<a href="'+ article.web_url +'">'+article.headline.main + '</a>' + 
+        '<p>' + article.snippet + '</p>' + 
+        '</li>');
+    }
+
+
+   //NYT error 
+}).fail(function(e)) {
+      $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    }); 
+
+return false;
+}
 
 $('#form-container').submit(loadData);
+
+
+

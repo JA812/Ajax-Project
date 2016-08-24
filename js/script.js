3814a56d397c48d82816934aa6dc2fd0e7d064b8
JA812 <johanna.asmuss@gmail.com>
2016-08-24 12:59:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,12 +36,12 @@ function loadData() {
 
 
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
 
     //load New York Times Articles
-    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=0fa786e945f94870bad1858afc173a78'
+    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=0fa786e945f94870bad1858afc173a78'
     $.getJSON(nytimesURL, function(data) {
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
     articles = data.response.docs;
